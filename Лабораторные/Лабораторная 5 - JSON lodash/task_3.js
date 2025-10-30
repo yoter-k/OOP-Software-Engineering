bedbdc4b90f48c251f8ea3f386df6f08e8e66448
Chumenko Igor <chumenk7@gmail.com>
2025-10-30 19:32:53 +0500

    b

--- Лабораторные/Лабораторная 5 - JSON lodash/task_3.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('./json/colors.json', 'utf8'));
-
-let names = data.map(item => Object.keys(item)[0]);
-let short = names.filter(name => name.length < 6);
-
-short.sort();
-console.log(JSON.stringify(short, null, 2));
